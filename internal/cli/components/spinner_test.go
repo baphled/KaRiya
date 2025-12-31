4eb615990dca3444c0fde28a38b3a437440d26b5
Development Team <dev@example.com>
2025-12-31 01:24:04 +0000

    test(components): add failing tests for spinner component
    Tests verify spinner creation, styling, and rendering.
    Currently fail - spinner component not yet implemented.

---
+++ internal/cli/components/spinner_test.go
@@ -0,0 +1,38 @@
+package components_test
+
+import (
+	"testing"
+
+	"github.com/baphled/kariya/internal/cli/components"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestSpinner(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Spinner Component Suite")
+}
+
+var _ = Describe("Spinner Component", func() {
+	Context("when creating a spinner", func() {
+		It("should create spinner with default style", func() {
+			spinner := components.NewSpinner()
+			Expect(spinner).NotTo(BeNil())
+		})
+
+		It("should have a View method that returns a string", func() {
+			spinner := components.NewSpinner()
+			view := spinner.View()
+			Expect(view).To(BeAssignableToTypeOf(""))
+		})
+	})
+
+	Context("when rendering the spinner", func() {
+		It("should render spinner with styled output", func() {
+			spinner := components.NewSpinner()
+			view := spinner.View()
+			Expect(view).NotTo(BeEmpty())
+		})
+	})
+})
+
